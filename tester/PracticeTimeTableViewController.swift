96b035cfcc7f12e65cde9c0371fac30ccf021493
John Cook <johncook@Johns-Macbook-Pro.local>
2015-01-03 16:26:12 -0600

    Added slide-over side menu

--- tester/PracticeTimeTableViewController.swift
+++ tester/PracticeTimeTableViewController.swift
@@ -22,6 +22,10 @@ import CoreData
         self.navigationController?.navigationBar.barTintColor = uicolorFromHex(0x2ecc71)
     }
     
+    @IBAction func onBurger() {
+        (tabBarController as TabBarController).sidebar.showInViewController(self.navigationController!, animated: true)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
